015d52be50f9692a8afa22b6874798d01841241f
Rick Voß <rivo@citservices.de>
2023-04-18 16:16:57 +0200

    initial setup

---
+++ out/extension.js
@@ -0,0 +1,63 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+const vscode = __importStar(require("vscode"));
+const hover_json_1 = __importDefault(require("./hover_cfg/hover.json"));
+function activate(context) {
+    vscode.window.showInformationMessage('Hello World from test!');
+    let disposable = vscode.commands.registerCommand('test.helloWorld', () => {
+        vscode.window.showInformationMessage('Hello World from test!');
+    });
+    context.subscriptions.push(disposable);
+    vscode.languages.registerHoverProvider('mccpu', {
+        provideHover(document, position, token) {
+            const range = document.getWordRangeAtPosition(position);
+            const word = document.getText(range);
+            const wordLower = word.toLowerCase();
+            let value = '';
+            hover_json_1.default.hover.forEach(currHover => {
+                if (currHover.aliases.some(alias => alias === wordLower)) {
+                    value = currHover.value.join('\n');
+                }
+            });
+            if (value === '') {
+                return;
+            }
+            return new vscode.Hover({
+                language: hover_json_1.default.language,
+                value: value
+            });
+        },
+    });
+}
+exports.activate = activate;
+// This method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
---
+++ src/extension.ts
@@ -0,0 +1,45 @@
+import * as vscode from 'vscode';
+
+import hover from './hover_cfg/hover.json';
+
+export function activate(context: vscode.ExtensionContext) {
+
+
+	vscode.window.showInformationMessage('Hello World from test!');
+
+	let disposable = vscode.commands.registerCommand('test.helloWorld', () => {
+		vscode.window.showInformationMessage('Hello World from test!');
+	});
+
+	context.subscriptions.push(disposable);
+
+	vscode.languages.registerHoverProvider('mccpu', {
+		provideHover(document, position, token) {
+			
+			const range = document.getWordRangeAtPosition(position);
+            const word = document.getText(range);
+			const wordLower = word.toLowerCase();
+
+			let value:string = '';
+			hover.hover.forEach(currHover => {
+				if(currHover.aliases.some(alias => alias === wordLower))
+				{
+					value = currHover.value.join('\n');
+				}
+			});
+
+			if(value === '')
+			{
+				return;
+			}
+
+			return new vscode.Hover({
+				language: hover.language,
+				value: value
+			});
+		},
+	});
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
